03e419e6b2448353119cab8752bc71837ead41cf
Lubomir Słotwiński <s33501@pjwstk.edu.pl>
2026-03-18 18:17:08 +0100

    Changed Writer

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Hello from branch!");
 int[] numbers = { 1, 2, 3, 4, 5 };
 
 if (numbers == null) throw new ArgumentException("Array is null");
